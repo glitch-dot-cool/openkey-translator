8ed8de02409d0a010d395c6a550dcd73c1f89f0b
alex gohorel <gohorelalex@gmail.com>
2025-01-20 22:25:17 -0500

    reduce rendering of KeyButton component

--- src/components/KeyButton.tsx
+++ src/components/KeyButton.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { memo } from "react";
 import { KeysInOpenKeyOrMusical, Result } from "../types";
 import { getColor, getResult } from "../utils";
 
@@ -7,13 +7,13 @@ export interface KeyButtonProps {
   setResult: React.Dispatch<React.SetStateAction<Result | undefined>>;
 }
 
-export const KeyButton = ({ _key, setResult }: KeyButtonProps) => {
-  const result = useMemo(() => getResult(_key), [_key]);
-  const color = useMemo(() => getColor({ key: _key }), [_key]);
+export const KeyButton = memo(({ _key, setResult }: KeyButtonProps) => {
+  const result = getResult(_key);
+  const color = getColor({ key: _key });
 
   return (
     <button onClick={() => setResult(result)} style={{ color }}>
       {_key}
     </button>
   );
-};
+});
